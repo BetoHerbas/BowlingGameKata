73b01a9b0d5f63fc2fd39664f9c140d09b5ca65b
BetoHerbas <betitoherbas@gmail.com>
2024-03-16 01:14:01 -0400

    Test all ones, should score 20

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,5 +1,14 @@
-class bowlingGame{
-
+class bowlingGame {
+  constructor() {
+    this.scoreValue = 0; 
+  }
+  score() {
+    return this.scoreValue; 
+  }
+  
+  roll(pins) {
+    this.scoreValue += pins; 
+  }
 }
 
 export default bowlingGame;
--- src/bowlingGame.test.js
+++ src/bowlingGame.test.js
@@ -5,4 +5,18 @@ describe("Bowling game test", () => {
     let g = new bowlingGame();
     expect(g).toBeInstanceOf(bowlingGame);
   });
+  it("Should score 0", () => {
+    let g = new bowlingGame();
+    for (let i = 0; i < 20; i++) {
+      g.roll(0);
+    }
+    expect(g.score()).toBe(0);
+  });
+  it("Test all ones, should score 20", () => {
+    let g = new bowlingGame();
+    for (let i = 0; i < 20; i++) {
+      g.roll(1);
+    }
+    expect(g.score()).toBe(20);
+  });
 });
